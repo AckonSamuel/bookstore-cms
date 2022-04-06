bea93aefdbb829b7814229d037e77f660b42e8d1
AckonSamuel <ackons433@gmail.com>
2022-04-07 00:04:40 +0100

    Import Redux Provider and my store in the main component of my react app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Navbar from './components/NavBar';
