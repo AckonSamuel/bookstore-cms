4b899a7ced75508b4a2e6502c356d4a14a3f2d51
AckonSamuel <ackons433@gmail.com>
2022-04-06 10:23:38 +0100

    Export Action Creators and reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,10 +1,27 @@
 const ADD_BOOK = 'ADD_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
 
-const addBook = () => ({
+const addBook = (book) => ({
   type: ADD_BOOK,
+  payload: book,
 });
 
-const removeBook = () => ({
+const removeBook = (book) => ({
   type: REMOVE_BOOK,
+  payload: book,
 });
+
+const bookReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [
+        ...state, action.payload,
+      ];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload.id);
+    default: return state;
+  }
+};
+
+export { addBook, removeBook };
+export default bookReducer;
