b1019ca2125578f0f4548275ae2909406183e8cf
AckonSamuel <ackons433@gmail.com>
2022-04-07 01:23:09 +0100

    Use useSelector to consume the state

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import Book from './Book';
 
 const BookList = () => {
-  const books;
+  const books = useSelector((state) => state.BookReducer);
 
   return (
     <>
       <ul className="allBooks">
         <li className="bookInfo">
-          {books.map((book) => (
-            <Book
-              category={book.category}
-              title={book.title}
-              author={book.author}
-              chapter={book.chapter}
-              completed={book.completed}
-              key={uuidv4()}
-            />
-          ))}
+          {
+         books.length !== 0 ? books.map((book) => (
+           <Book
+             category={book.category}
+             title={book.title}
+             author={book.author}
+             chapter={book.chapter}
+             completed={book.completed}
+             key={uuidv4()}
+           />
+         )) : <h4>NO BOOKS AVAILABLE</h4>
+}
         </li>
       </ul>
     </>
