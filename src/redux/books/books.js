04f8570e1421fa19c1ddb2b4c31ba38355aaafd4
AckonSamuel <ackons433@gmail.com>
2022-04-07 18:30:00 +0100

    Dispatch actions and refactor reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,3 +1,5 @@
+import { saveBook, loadBooks, removeBook } from '../../components/api_var';
+
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const ADDED_BOOK = 'bookstore/books/ADDED_BOOK';
 
@@ -7,43 +9,54 @@ const LOADED_BOOKS = 'bookstore/books/LOADED_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 const REMOVED_BOOK = 'bookstore/books/REMOVED_BOOK';
 
+const AddBook = (book) => (dispatch) => {
+  dispatch({ type: ADD_BOOK });
+  saveBook(book).then((status) => {
+    if (status === 201 || status === 200) {
+      dispatch({ type: ADDED_BOOK, payload: book });
+    }
+  });
+};
 
-const initialState = [
-  {
-    title: 'The Nature of Nature',
-    author: 'Enric Sala',
-  },
-  {
-    title: 'The Design of Everyday Things',
-    author: 'Donald A. Norman',
-  },
-  {
-    title: 'The Art of War',
-    author: 'Sun-Tsu',
-  },
-];
+const BookLoad = () => (dispatch) => {
+  dispatch({ type: LOAD_BOOKS });
+  loadBooks().then((books) => dispatch({
+    type: LOADED_BOOKS,
+    payload: books,
+  }));
+};
 
-const AddBook = (book) => ({
-  type: ADD_BOOK,
-  payload: book,
-});
+const RemoveBook = (index, id) => (dispatch) => {
+  dispatch({ type: REMOVE_BOOK });
+  removeBook(id).then((status) => {
+    if (status === 201 || status === 200) dispatch({ type: REMOVED_BOOK, payload: index });
+  });
+};
 
-const RemoveBook = (book) => ({
-  type: REMOVE_BOOK,
-  payload: book,
-});
+const initialState = {
+  books: [
+  ],
+};
 
 const BookReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_BOOK:
-      return [
-        ...state, action.payload,
-      ];
+    case LOAD_BOOKS:
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload.id);
-    default: return state;
+    case ADD_BOOK:
+      return { ...state, waiting: true };
+    case ADDED_BOOK:
+      return { ...state, books: [...state.books, action.payload], waiting: false };
+    case LOADED_BOOKS:
+      return { ...state, books: [...state.books, ...action.payload] };
+    case REMOVED_BOOK:
+      return {
+        ...state,
+        books: [...state.books.slice(0, action.payload), ...state.books.slice(action.payload + 1)],
+      };
+    default:
+      return state;
   }
 };
 
-export { AddBook, RemoveBook };
+export { AddBook, RemoveBook, BookLoad };
 export default BookReducer;
