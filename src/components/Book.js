ffd70518c03d68751f7606d05198bb7e930c1edc
AckonSamuel <ackons433@gmail.com>
2022-04-07 01:57:29 +0100

    Dispatch remove action

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
+import { RemoveBook } from '../redux/books/books';
 import Button from './Button';
 
-const Book = ({ title, author }) => (
-  <div className="book-container">
-    <>
-      <h2 id="title">{title}</h2>
-      <p id="author">{author}</p>
+const Book = ({ title, author }) => {
+  const dispatch = useDispatch();
 
-      <div className="btn-container">
-        <Button buttonType="button" text="Remove" id="btn-remove" />
-        <Button buttonType="button" text="Edit" id="btn-edit" />
-      </div>
-    </>
-  </div>
-);
+  return (
+    <div className="book-container">
+      <>
+        <h2 id="title">{title}</h2>
+        <p id="author">{author}</p>
+
+        <div className="btn-container">
+          <Button buttonType="button" text="Remove" id="btn-remove" onClick={(book) => dispatch(RemoveBook(book))} />
+          <Button buttonType="button" text="Edit" id="btn-edit" />
+        </div>
+      </>
+    </div>
+  );
+};
 
 Book.propTypes = {
   title: PropTypes.string.isRequired,
