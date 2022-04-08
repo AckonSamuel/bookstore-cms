684d1d59ad794cc45758c3da334dbe4903ec42ff
AckonSamuel <ackons433@gmail.com>
2022-04-08 15:55:23 +0100

    Add styles for nav bar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ImUser } from 'react-icons/im';
 import './sytles/navbar.css';
+import imuser from './sytles/react-icons';
 
 const Navbar = () => (
   <nav>
     <span id="logo">Bookstore</span>
     <>
-      <Link to="/">Books</Link>
-      <Link to="/categories">Categories</Link>
+      <span className="BOOKS"><Link to="/">Books</Link></span>
+      <span className="CATEGORIES"><Link to="/categories">Categories</Link></span>
     </>
 
-    <ImUser />
+    <ImUser style={imuser} />
   </nav>
 );
 
---
+++ src/components/sytles/react-icons.js
@@ -0,0 +1,10 @@
+const imuser = {
+  width: '2.813rem',
+  height: '2.813rem',
+  margin: '0 0 0 41.5rem',
+  padding: '0.875rem',
+  border: 'solid 1px var(--grey-border)',
+  color: 'var(--azure)',
+};
+
+export default imuser;
