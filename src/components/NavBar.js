61bd1ee86e8678e26df4c9c1f70a39f062121f2d
AckonSamuel <ackons433@gmail.com>
2022-04-08 15:29:45 +0100

    Import css file for navbar in NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ImUser } from 'react-icons/im';
+import './sytles/navbar.css';
 
 const Navbar = () => (
   <nav>
-    <h1 id="logo">Bookstore</h1>
-    <ImUser />
+    <span id="logo">Bookstore</span>
     <>
       <Link to="/">Books</Link>
       <Link to="/categories">Categories</Link>
     </>
+
+    <ImUser />
   </nav>
 );
 
