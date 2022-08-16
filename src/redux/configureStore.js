e93902f60d32a0dbb0bb79c9475c80ff5b2ec95b
Ackon Samuel <ackons433@gmail.com>
2022-08-16 19:09:02 +0000

    hot fix

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,12 @@
-import { combineReducers } from 'redux';
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 import BookReducer from './books/books';
 import CategoriesReducer from './categories/categories';
 
-const reducers = combineReducers({
-  BookReducer,
-  CategoriesReducer,
+const store = configureStore({
+  reducer: {
+    BookReducer,
+    CategoriesReducer,
+  }
 });
-
-const store = configureStore(reducers)
 
 export default store;
